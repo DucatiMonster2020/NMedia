dfbecbfc352dd1adcb065a92b6e2c9ca12c23f31
Sergey Arefin <arefin-sergey92@mail.ru>
2025-02-25 19:16:20 +0300

    add files

---
+++ app/src/main/java/ru/netology/nmedia/view/MainActivity.kt
@@ -0,0 +1,45 @@
+package ru.netology.nmedia.view
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.ActivityPageBinding
+import ru.netology.nmedia.post.countFormat
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                heart.setImageResource(
+                    if (post.likedByMe) {
+                        R.drawable.liked_heart
+                    } else {
+                        R.drawable.heart_outline
+                    }
+                )
+                if (post.likedByMe) post.countLikes++ else post.countLikes--
+                countLikes.text = post.countLikes.toString()
+
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
+                countLikes.text = countFormat(post.countLikes)
+                countShare.text = countFormat(post.countShare)
+                countView.text = countFormat(post.countView)
+            }
+        }
+        binding.heart.setOnClickListener {
+            viewModel.like()
+        }
+        binding.share.setOnClickListener {
+            viewModel.share()
+        }
+    }
+}
